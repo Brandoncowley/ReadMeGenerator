d81e05fc42cd7ac9d4aea68ef7cd8b9435d42be3
Brandon Cowley <brandon.cowley@gmail.com>
2021-05-23 18:53:53 -0700

    code complete, need to record demo and paste to file

--- README.md
+++ README.md
@@ -1,34 +1,37 @@
 
-    # README Generator
-      serdghn
+    # ReadMe Generator
+    #### Github: https://github.com/Brandoncowley
+    Video walkthrough for usage: 
+
+    ## Table of Contents
+    1. [Github](#github)
+    2. [Email](#email)
+    3. [Description](#description)
+    4. [License](#license)
+    5. [Install](#install)
+    6. [Testing](#test)
+    7. [Repo](#repo)
+    8. [Contribution](#contribution)
 
     
-    ## Table of Contents
-    1. [github](#github)
-    2. [email](#email)
-    3. [project](#project)
-    4. [description](#description)
-    5. [license](#license)
-    6. [install](#install)
-    7. [test](#test)
-    8. [repo](#repo)
-    9. [contribution](#contribution)
+    ## Description: 
+        This project helps users create a ReadMe file
+        
+    ## Installation commands required:
+    Use the following code in your code for installation:
+    ```    
+    - npm install inquirer
+    ```
 
-   
-    ## Github
-        asedfghj
-    ## email
-        fhgdfs
-    ## desc
-        nhfvgcd
-    ## lic
+    ## Testing information
+        I command you to run tests!
+    ## Repo
+        Brandon Cowley
+
+    ##### Licensed with:
         [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-    ## inst
-        fgdbvdcaszdrvfb
-    ## test
-        asdbsdrv
-    ## Grepo
-        ardhbdfhmnsbevsv
-    ## cont
-        WARSDFHBDRTVGCSDRXVGSCDRF
+    ## Contributor:
+        Brandon Cowley
+    ## email
+    You can contact the creator with questions regarding the generator at brandon.cowley@gmail.com
     
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const questions = [
     {
         type: 'input',
         name: 'github',
-        message: 'What is your GitHub username?',
+        message: 'What is your GitHub username? (include your https!)',
     },
     {
         type: 'input',
@@ -23,7 +23,7 @@ const questions = [
     {
         type: 'input',
         name: 'description',
-        message: 'Write a brief description of your project:',
+        message: 'Give a description of your project for the user:',
     },
     {
         type: 'list',
@@ -52,51 +52,55 @@ const questions = [
         message: 'What should the user know about repo contribution?',
     },
   ]
-  
+  //Start the question and response flow for the user
   inquirer.prompt(questions).then((response)=> {
-    
+    //display badges for license selection
     if (response.license === 'ISC') {
         response.license = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
     }
     if (response.license === 'Mozilla') {
         response.license = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
-    } else {
+    }
+    if (response.license === 'MIT') {
         response.license = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
     }
-    
+    //PAGE OUTPUT LAYOUT
     const result = `
-    # README Generator
-      ${response.project}
+    # ${response.project}
+    #### Github: ${response.github}
+    Video walkthrough for usage: 
+
+    ## Table of Contents
+    1. [Github](#github)
+    2. [Email](#email)
+    3. [Description](#description)
+    4. [License](#license)
+    5. [Install](#install)
+    6. [Testing](#test)
+    7. [Repo](#repo)
+    8. [Contribution](#contribution)
 
     
-    ## Table of Contents
-    1. [github](#github)
-    2. [email](#email)
-    3. [project](#project)
-    4. [description](#description)
-    5. [license](#license)
-    6. [install](#install)
-    7. [test](#test)
-    8. [repo](#repo)
-    9. [contribution](#contribution)
-
-   
-    ## Github
-        ${response.github}
-    ## email
-        ${response.email}
-    ## desc
+    ## Description: 
         ${response.description}
-    ## lic
-        ${response.license}
-    ## inst
-        ${response.install}
-    ## test
+        
+    ## Installation commands required:
+    Use the following code in your code for installation:
+    \`\`\`    
+    - ${response.install}
+    \`\`\`
+
+    ## Testing information
         ${response.test}
-    ## Grepo
+    ## Repo
         ${response.repo}
-    ## cont
+
+    ##### Licensed with:
+        ${response.license}
+    ## Contributor:
         ${response.contribution}
+    ## email
+    You can contact the creator with questions regarding the generator at ${response.email}
     `
 
     fs.writeFile("README.md", result, (err) =>
@@ -104,4 +108,12 @@ const questions = [
 
   }
   )
+  
+  function generateMarkdown(data) {
+    return `# ${data.title}
+  
+  `;
+  }
+  
+  module.exports = generateMarkdown;
   
